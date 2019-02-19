ee54db26ee1685d9f563066644bfde717dfafe34
William Duckworth <general@wduckworth.me.uk>
2019-02-19 15:38:06 +0000

    Added exercise code
    I added the exercise code to ask for a number and then return a countdown from that number to zero

--- 2_3_countdown.py
+++ 2_3_countdown.py
@@ -6,7 +6,11 @@ Created on Jan 8, 2019
 '''
 
 def main():
-  print('Hello World')
+  n = int(input('Starting number for countdown? '))
+  while n >= 0:
+    print(n, end='')
+    n -= 1
+  print()
 
 if __name__ = '__main__':
   main()
