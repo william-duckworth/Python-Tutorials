1469f2cf6076bee4c9d962e3af9cfd90da1541d8
William Duckworth <general@wduckworth.me.uk>
2019-02-19 15:30:53 +0000

    Added exercise code
    I added the code for the exercise to ask the user to enter a year and then return if it is a leap year or not

--- 2_2_leap_year.py
+++ 2_2_leap_year.py
@@ -6,7 +6,16 @@ Created on Jan 8, 2019
 '''
 
 def main():
-  print('Hello World')
+  year = int(input('Enter a year? '))
+  if year > 0:
+    if year%400 == 0:
+      print('leap year')
+    elif year%4==0 and year%100!=0:
+      print('leap year')
+    else:
+      print('not a leap year')
+  else:
+    print('Must be greater than zero')
 
 if __name__ = '__main__':
   main()
