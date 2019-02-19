1f4febf4e3cf8d750e172a111f832d305f823c9f
William Duckworth <general@wduckworth.me.uk>
2019-02-19 15:06:20 +0000

    Added exercise code
    I added the code for the exersie to ask the user for a number and then return the classification back to them

--- 1_5_number_classifier.py
+++ 1_5_number_classifier.py
@@ -6,7 +6,19 @@ Created on Jan 8, 2019
 '''
 
 def main():
-  print('Hello World')
+    x = int(input('Think of a number? '))
+  if x < 0:
+    print('negative')
+  elif x > 0:
+    if x >= 1000:
+      print('large', end='')
+    elif x < 100:
+      print('small', end='')
+    else:
+      print('medium', end='')
+    print('positive')
+else:
+    print('zero')
 
 if __name__ = '__main__':
   main()
