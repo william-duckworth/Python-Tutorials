795fe494a8d8164388d3b06ccd50c982149aac2c
William Duckworth <general@wduckworth.me.uk>
2019-02-19 15:45:29 +0000

    Added exercise code
    I added the code for the exercise

--- 2_4_line_reader.py
+++ 2_4_line_reader.py
@@ -6,7 +6,14 @@ Created on Jan 8, 2019
 '''
 
 def main():
-  print('Hello World')
+  running = True
+  while running:
+    cmd = input('Enter a command (blank or "exit" to stop)? ')
+    cmd = cmd.strip().lower()
+    if not cmd or cmd == 'exit':
+      running = False
+    else:
+      print('Command entered:', cmd)
   
 if __name__ = '__main__':
   main()
