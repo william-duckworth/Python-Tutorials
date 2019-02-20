ef7783c0098db9bed25957989a1be346e8d4083d
William Duckworth <general@wduckworth.me.uk>
2019-02-20 09:04:34 +0000

    Exercise Code
    I added the code for the exercise for dates

--- 3_2_dates.py
+++ 3_2_dates.py
@@ -5,8 +5,12 @@ Created on Jan 8, 2019
 @author: William Duckworth
 '''
 
+from datetime import datetime as dt
+
 def main():
-  print('Hello World')
+  dob = input('Enter your DOB in the format dd/mm/yyyy?')
+  dt_dob = dt.strptime(dob, '%d/%m/%Y')
+  print(dt_dob.strftime('%A %B %d, %Y'))
 
 if __name__ = '__main__':
   main()
