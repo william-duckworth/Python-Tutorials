d881624ab1f6e546772e691bc29eb02f55c58e21
William Duckworth <general@wduckworth.me.uk>
2019-02-20 08:50:30 +0000

    Exercise Code
    I added the code for the exercise to the file to get the launch angle and the range

--- 3_1_projectile_paths.py
+++ 3_1_projectile_paths.py
@@ -5,8 +5,28 @@ Created on Jan 8, 2019
 @author: William Duckworth
 '''
 
+import math
+
+GRAVITY = 9.81
+VELOCITY = 100.0
+
 def main():
-  print('Hello World')
+  theta()
+  range()
+  
+def theta():
+  line = input('how far to the target [up to 1000]? ')
+  distance = float(line)
+
+  theta = math.asin(distance * GRAVITY / VELOCITY**2) / 2
+  print('launch angle is', math.degrees(theta))
+  
+def range():
+  line = input('what launch angle have you set? ')
+  theta = math.radians(float(line))
+
+  distance = 2 * VELOCITY**2 * math.cos(theta) * math.sin(theta) / GRAVITY
+  print('your range is', distance)
   
 if __name__ == '__main__':
   main()
