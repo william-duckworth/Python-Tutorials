a0615f37be773433a65332834be8421c3f6b0e86
william-duckworth <>
2019-02-21 11:23:34 +0000

    Added the next lot of code to strip away the white space

--- 4_1_number_reader.py
+++ 4_1_number_reader.py
@@ -12,12 +12,22 @@ def main():
     number2 = read_number_prompt('Enter a number? ')
     print(number2, ' squared is ', number2**2)
 
+    number3 = read_number_strip('Enter a number? ')
+    print(number3, ' squared is ', number3**2)
+
 def read_number():
     line = input('Enter a number? ')
     return int(line)
 
 def read_number_prompt(prompt):
     line = input(prompt)
+    return int(line)
+
+def read_number_strip(prompt):
+    asking = True
+    while asking:
+        line = input(prompt).strip()
+        asking = not line.isdigit()
     return int(line)
 
 if __name__ == "__main__":
